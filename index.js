e123a85a0f988bdde4b64537a2d87980fd3de7b6
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2023-05-19 18:22:57 +0600

    commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 
 import router from "./routes/products.js";
 import ordersRouter from "./routes/orders.js";
@@ -30,7 +29,7 @@ mongoose.connection.on("connect", () => {
 
 // middleware
 app.use(cors());
-app.use(cookieParser());
+
 app.use(express.json());
 
 app.use("/api/products", router);
