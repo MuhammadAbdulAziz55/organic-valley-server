f731ebf5af8e0c3d7b086e07548bf62a9128d30b
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2023-05-20 09:59:17 +0600

    change lean() from create product api

--- controllers/product.js
+++ controllers/product.js
@@ -3,7 +3,7 @@ import Product from "../models/Product.js";
 export const createProduct = async (req, res, next) => {
   const newProduct = new Product(req.body);
   try {
-    const savedProduct = await newProduct.save().lean();
+    const savedProduct = await newProduct.save();
     res.status(200).json(savedProduct);
   } catch (err) {
     next(err);
